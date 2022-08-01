f2281c6c3a74d44987da824a459152849bfa0496
TungVu858 <tungvt858@gmail.com>
2022-08-01 10:58:23 +0700

    1/8-tung validate thay dổi mật khẩu

--- src/app/component/user/change-password/change-password.component.ts
+++ src/app/component/user/change-password/change-password.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {UserService} from "../../../service/user.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {AuthenticationService} from "../../../service/authentication.service";
@@ -15,9 +15,9 @@ export class ChangePasswordComponent implements OnInit {
   id = parseInt(localStorage.getItem("ID"));
   username = localStorage.getItem("USERNAME")
   userForm: FormGroup = new FormGroup({
-    password: new FormControl(),
-    confirmPassword: new FormControl(),
-    currentPassword : new FormControl()
+    password: new FormControl("",[Validators.required, Validators.minLength(6), Validators.maxLength(32)]),
+    confirmPassword: new FormControl("",[Validators.required, Validators.minLength(6), Validators.maxLength(32)]),
+    currentPassword : new FormControl("",[Validators.required, Validators.minLength(6), Validators.maxLength(32)])
   })
   constructor(private userService: UserService, private activatedRoute: ActivatedRoute, private router: Router,private authenticationService: AuthenticationService) { }
 
@@ -50,6 +50,18 @@ export class ChangePasswordComponent implements OnInit {
         document.getElementById("fail").style.visibility = "visible"
       }
     }
+  }
+
+  get currentPassword(){
+    return this.userForm.get('currentPassword');
+  }
+
+  get password(){
+    return this.userForm.get('password');
+  }
+
+  get confirmPassword(){
+    return this.userForm.get('confirmPassword');
   }
   ngOnInit(): void {
   }
