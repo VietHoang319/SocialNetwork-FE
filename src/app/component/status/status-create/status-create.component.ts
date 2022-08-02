b57a1272966bdd8dd84d1752c012a2c054307ddb
chandoithatsu <100838893+Afdfghjhgcfghvh@users.noreply.github.com>
2022-08-02 10:39:58 +0700

    2/8 FE hung tao status ok

--- src/app/component/status/status-create/status-create.component.ts
+++ src/app/component/status/status-create/status-create.component.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
 import {StatusService} from "../../../service/status.service";
-import {Status} from "../../../model/status";
-import {User} from "../../../model/user";
-import {allowMangle} from "@angular-devkit/build-angular/src/utils/environment-options";
+import {finalize, Observable} from "rxjs";
+import {ImageService} from "../../../service/image.service";
+import {AngularFireStorage} from "@angular/fire/compat/storage";
 
 @Component({
   selector: 'app-status-create',
@@ -11,13 +11,20 @@ import {allowMangle} from "@angular-devkit/build-angular/src/utils/environment-o
   styleUrls: ['./status-create.component.css']
 })
 export class StatusCreateComponent implements OnInit {
+  avatarCurrentUser = localStorage.getItem("AVATAR")
   statuz: any;
+  selectedImages: any[] = [];
+  imgs: string[] = [];
+  newSelectedImages: [];
+  fileInfos: Observable<any>;
   statusForm: FormGroup = new FormGroup({
     content: new FormControl(''),
     status: new FormControl(''),
   });
 
-  constructor(private statusService: StatusService) {
+  constructor(private statusService: StatusService,
+              private iamgeService: ImageService,
+              private storage: AngularFireStorage) {
   }
 
   ngOnInit(): void {
@@ -31,15 +38,60 @@ export class StatusCreateComponent implements OnInit {
         id: localStorage.getItem('ID'),
       }
     }
-    if (this.statuz.content == "") {
+    if (this.statuz.content == "" && this.imgs.length == 0) {
     } else {
       if (this.statuz.status == 0) {
         this.statuz.status = 1
       }
       this.statusService.save(this.statuz).subscribe((data) => {
+        for (let item of this.imgs) {
+          const images = {
+            status: {
+              id: data.id
+            },
+            image: item
+          }
+          this.iamgeService.save(images).subscribe(() => {
+          });
+        }
         this.statusForm.reset();
       }, error => {
       });
+    }}
+
+  // upload ảnh
+  async upload() {
+    if (this.newSelectedImages.length !== 0) {
+      for (let i = 0; i < this.newSelectedImages.length; i++) {
+        let selectedImage = this.newSelectedImages[i];
+        var n = Date.now();
+        const filePath = `RoomsImages/${n}`;
+        const fileRef = this.storage.ref(filePath);
+        await this.storage.upload(filePath, selectedImage).snapshotChanges().pipe(
+          finalize(() => {
+            fileRef.getDownloadURL().subscribe(url => {
+              console.log(url);
+              this.imgs.push(url);
+            });
+          })
+        ).subscribe(() => {
+        });
+      }
+    }
+  }
+
+  // Chọn ảnh
+  selectFile(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      this.newSelectedImages = event.target.files;
+      for (let i = 0; i < event.target.files.length; i++) {
+        this.selectedImages.push(event.target.files[i]);
+      }
+      this.upload()
+    } else {
+      this.selectedImages = [];
     }
   }
 }
