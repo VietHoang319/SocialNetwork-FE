52be728ede1ec257f1a250decce0f0cd60cd9c7a
chandoithatsu <100838893+Afdfghjhgcfghvh@users.noreply.github.com>
2022-08-04 16:20:50 +0700

    non

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,8 @@ import {StatusOwnerListComponent} from "./component/status/status-owner-list/sta
 import { RelationshipComponent } from './component/user/relationship/relationship.component';
 import { ListFriendComponent } from './component/block/list-friend/list-friend.component';
 import { StatusNewFeedComponent } from './component/status/status-new-feed/status-new-feed.component';
+import { CommenListComponent } from './component/comment/commen-list/commen-list.component';
+import { CommentCreateComponent } from './component/comment/comment-create/comment-create.component';
 
 @NgModule({
   declarations: [
@@ -38,6 +40,8 @@ import { StatusNewFeedComponent } from './component/status/status-new-feed/statu
     ChangePasswordComponent,
     PostsComponent,
     StatusUpdateComponent,
+    CommenListComponent,
+    CommentCreateComponent,
     RelationshipComponent,
     ListFriendComponent,
     StatusListComponent,
