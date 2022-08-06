64e06da379e95b6ed7141d622c6aa1bfda9ce7c3
VietHoang319 <arishoang319@gmail.com>
2022-08-06 11:23:42 +0700

    6/8 - Hoàng - get status

--- src/app/component/status/status-detail/status-detail.component.ts
+++ src/app/component/status/status-detail/status-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {StatusService} from "../../../service/status.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Image} from "../../../model/image";
 
 @Component({
   selector: 'app-status-detail',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./status-detail.component.css']
 })
 export class StatusDetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  statusId: any
+  status: any
+  listImage: Image[] = []
+  constructor(private statusService: StatusService, private activatedRoute: ActivatedRoute) {
   }
 
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((param) => {
+      this.statusId = param.get("id");
+      this.getStatus(this.statusId)
+    })
+  }
+
+  getStatus(id) {
+    this.statusService.getById(id).subscribe(data => {
+      this.status = data[0][0]
+      this.listImage = data[1]
+    }, error => {
+      console.log(error)
+    })
+  }
 }
