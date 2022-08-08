e2c24d0507b06922a7d10d5a4bbca6f254d61241
TungVu858 <tungvt858@gmail.com>
2022-08-08 08:44:42 +0700

    8/8-tung done hiển thị status

--- src/app/component/status/status-owner-list/status-owner-list.component.ts
+++ src/app/component/status/status-owner-list/status-owner-list.component.ts
@@ -6,9 +6,6 @@ import {RelationshipService} from "../../../service/relationship.service";
 import {Status} from "../../../model/status";
 import {Image} from "../../../model/image";
 import {ImageService} from "../../../service/image.service";
-import {error} from "@angular/compiler-cli/src/transformers/util";
-import {HttpStatusCode} from "@angular/common/http";
-import {timeoutWith} from "rxjs";
 
 
 @Component({
@@ -21,50 +18,45 @@ export class StatusOwnerListComponent implements OnInit {
   currentId: any;
   id: any;
   relationship: any;
-  relationshipStatus : any;
   statuz: Status [] = [];
-  img : Image [] = [];
-  image : Image [] = [];
+  img: Image [] = [];
+  image: Image [] = [];
+
   constructor(private statusService: StatusService, private activatedRoute: ActivatedRoute,
               private fb: FormBuilder, private router: Router, private relationshipService: RelationshipService,
-              private imageService : ImageService) {
+              private imageService: ImageService) {
 
   }
 
 
-
   ngOnInit(): void {
     this.currentId = localStorage.getItem("ID")
-    this.relationshipService.findAllFriendListByUserId(this.currentId).subscribe(data => {
-      this.relationship = data;
-    })
     this.activatedRoute.paramMap.subscribe((param) => {
       this.id = param.get("id");
     })
-    this.relationshipService.getRelationship(this.currentId,this.id).subscribe(data => {
-      this.relationshipStatus = data;
+    this.relationshipService.getRelationship(this.currentId, this.id).subscribe(data => {
+      this.relationship = data;
       this.statusService.findAllByOwnerId(this.id).subscribe((status) => {
         this.statuz = status;
         for (let i = 0; i <= status.length; i++) {
           if (this.currentId == this.id) {
             this.statuses.push(status[i])
             this.statuses.push(this.statuz[0][i])
-            this.imageService.findByIdStatus(this.statuz[1][i][i].id).subscribe((images) =>{
+            this.imageService.findByIdStatus(this.statuz[1][i][i].id).subscribe((images) => {
               this.img.push(images)
             })
-          }
-          else {
-            if ((this.statuz[0][i].status == 1 || this.statuz[0][i].status == 3) && this.relationshipStatus.status == 2){
+          } else {
+            console.log(this.relationship)
+            if (this.statuz[0][i].status == 1 && this.relationship == null) {
+              console.log(this.relationship)
               this.statuses.push(this.statuz[0][i])
-              this.imageService.findByIdStatus(this.statuz[1][i][i].id).subscribe((images) =>{
+              this.imageService.findByIdStatus(this.statuz[1][i][i].id).subscribe((images) => {
                 this.img.push(images)
               })
             }
-            else if (this.statuz[0][i].status == 1 ){
-              alert(2)
-              console.log(this.relationshipStatus)
+            if ((this.statuz[0][i].status == 1 && this.relationship.status == 2) || (this.statuz[0][i].status == 3 && this.relationship.status == 2)) {
               this.statuses.push(this.statuz[0][i])
-              this.imageService.findByIdStatus(this.statuz[1][i][i].id).subscribe((images) =>{
+              this.imageService.findByIdStatus(this.statuz[1][i][i].id).subscribe((images) => {
                 this.img.push(images)
               })
             }
