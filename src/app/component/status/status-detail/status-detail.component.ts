161bb29e4f399b4cb8b624f480dcef2090f524e6
VietHoang319 <arishoang319@gmail.com>
2022-08-09 11:34:28 +0700

    9/8 - Hoàng - thêm like status ở status-detail

--- src/app/component/status/status-detail/status-detail.component.ts
+++ src/app/component/status/status-detail/status-detail.component.ts
@@ -5,6 +5,7 @@ import {Image} from "../../../model/image";
 import {CommentService} from "../../../service/comment.service";
 import {Comment} from "../../../model/comment";
 import {RelationshipService} from "../../../service/relationship.service";
+import {LikeStatusService} from "../../../service/like-status.service";
 
 @Component({
   selector: 'app-status-detail',
@@ -20,12 +21,14 @@ export class StatusDetailComponent implements OnInit {
   listImage: Image[] = []
   listCommentOfStatus: any[] = []
   listCommentOfComment: any[] = []
+  numberOfLikeOfStatus: any
+  likeStatuses: any
 
   constructor(private statusService: StatusService,
               private activatedRoute: ActivatedRoute,
               private commentService: CommentService,
               private relationshipService : RelationshipService,
-              private router: Router) {
+              private router: Router, private likeStatusService : LikeStatusService) {
   }
 
   ngOnInit(): void {
@@ -42,8 +45,14 @@ export class StatusDetailComponent implements OnInit {
       this.status = data[0][0]
       this.relationshipService.getRelationship(this.currentUserId, this.status.owner.id).subscribe(data => {
         this.relationship = data
+        this.likeStatusService.check(this.status.id, this.currentUserId).subscribe(data => {
+          this.status.isLiked = data;
+        }, error => (
+          console.log("err", error)
+        ))
       })
       this.listImage = data[1]
+      this.numberOfLikeOfStatus = data[2][0]
     }, error => {
       console.log(error)
     })
@@ -115,5 +124,17 @@ export class StatusDetailComponent implements OnInit {
     document.getElementsByClassName("child-comment-content")[index].style.display = "block"
     // @ts-ignore
     document.getElementsByClassName("child-comment-update")[index].style.display = "none"
+  }
+
+  likeStatus(id: any) {
+    this.likeStatusService.likeStatus(id, this.currentUserId).subscribe(data => {
+      this.likeStatuses = data
+      this.status.isLiked = !this.status.isLiked
+      if (this.status.isLiked == true) {
+        this.numberOfLikeOfStatus += 1
+      } else {
+        this.numberOfLikeOfStatus -= 1
+      }
+    })
   }
 }
