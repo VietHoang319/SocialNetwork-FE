562c533d1d231a92c93af822ada9951b1e0f1f0f
VietHoang319 <arishoang319@gmail.com>
2022-08-09 22:50:56 +0700

    9/8 - Hoàng - done sửa comment

--- src/app/component/block/status-list/status-list.component.ts
+++ src/app/component/block/status-list/status-list.component.ts
@@ -86,15 +86,30 @@ export class StatusListComponent implements OnInit {
       content: this.statusForm.value.content,
       status: this.statusForm.value.status,
     }
-
     console.log(status);
-    // @ts-ignore
-    this.statusService.edit(this.statusz.id, status).subscribe((data) => {
-      this.toast.success({detail: "Thông Báo", summary: "Sửa bài đăng thành công", duration: 3000})
-      this.reloadCurrentRoute()
-    }, error => {
-      console.log(error);
-    })
+    if (status.content == "" && this.imgs.length == 0) {
+
+    } else {
+      // @ts-ignore
+      this.statusService.edit(this.statusz.id, status).subscribe((data) => {
+        this.toast.success({detail: "Thông Báo", summary: "Sửa bài đăng thành công", duration: 3000})
+        for (let item of this.imgs) {
+          const images = {
+            status: {
+              id: data.id
+            },
+            image: item
+          }
+          this.imageService.save(images).subscribe(() => {
+          });
+        }
+        this.statusForm.reset();
+        this.imgs = []
+        this.reloadCurrentRoute()
+      }, error => {
+        console.log(error);
+      })
+    }
   }
 
   deleteImg(i: any) {
@@ -161,5 +176,4 @@ export class StatusListComponent implements OnInit {
       this.selectedImages = [];
     }
   }
-
 }
